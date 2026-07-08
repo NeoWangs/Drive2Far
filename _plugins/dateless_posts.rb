856d3ef33cd443a904bacd39d9bf21c8767c4720
Eason Wang <neoxone2@gmail.com>
2026-07-08 16:53:29 +0800

    支持 Obsidian 阅读

--- _plugins/dateless_posts.rb
+++ _plugins/dateless_posts.rb
@@ -21,6 +21,34 @@ module Jekyll
       @post_asset_url
     end
   end
+
+  module ObsidianPostAssetUrls
+    IMAGE_SRC_PATTERN = %r{(<img\b[^>]*\bsrc=(["']))([^"']+)(\2)}i.freeze
+
+    module_function
+
+    def rewrite(doc)
+      slug = doc.data["slug"].to_s
+      return if slug.empty? || doc.output.to_s.empty?
+
+      asset_dir = File.join(File.dirname(doc.path), slug)
+      return unless File.directory?(asset_dir)
+
+      doc.output = doc.output.gsub(IMAGE_SRC_PATTERN) do
+        prefix = Regexp.last_match(1)
+        src = Regexp.last_match(3)
+        suffix = Regexp.last_match(4)
+
+        next Regexp.last_match(0) unless src.start_with?("#{slug}/")
+
+        relative_asset_path = src.delete_prefix("#{slug}/")
+        next Regexp.last_match(0) if relative_asset_path.include?("..")
+        next Regexp.last_match(0) unless File.file?(File.join(asset_dir, relative_asset_path))
+
+        "#{prefix}#{File.join(doc.url, relative_asset_path)}#{suffix}"
+      end
+    end
+  end
 end
 
 Jekyll::Hooks.register :site, :post_read do |site|
@@ -44,4 +72,10 @@ Jekyll::Hooks.register :site, :post_read do |site|
       site.static_files << Jekyll::PostAssetFile.new(site, site.source, dir, name, url)
     end
   end
+end
+
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  next unless doc.collection.label == "posts"
+
+  Jekyll::ObsidianPostAssetUrls.rewrite(doc)
 end
